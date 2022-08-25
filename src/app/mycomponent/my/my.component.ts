1b3af778495f9f2fad14a93973f5e3a3dfd814e0
Ronakmer <106221380+Ronakmer@users.noreply.github.com>
2022-08-25 12:42:35 +0530

    Add files via upload

--- src/app/mycomponent/my/my.component.ts
+++ src/app/mycomponent/my/my.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import{mys} from "../../mys";
 
 @Component({
   selector: 'app-my',
@@ -9,29 +7,7 @@ import{mys} from "../../mys";
 })
 export class MyComponent implements OnInit {
 
-  my:mys[];
-  constructor() { 
-    this.my=[
-     {
-      sno:1,
-      title:"asd",
-      desc:"zxc",
-      active:true
-     },
-     {
-      sno:2,
-      title:"qwe",
-      desc:"dfg",
-      active:true
-     },
-     {
-      sno:3,
-      title:"ert",
-      desc:"cvb",
-      active:true
-     },
-    ]
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
